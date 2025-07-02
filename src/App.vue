a65cdb97843db7ec06810e5f938291e1054d8d05
henrique-furtado47 <henrique.furtadoifc@gmail.com>
2025-07-02 09:26:01 -0300

    Primeiro Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {  RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
+import HeaderPortfolio from './components/HeaderPortfolio.vue'
+import FooterPortfolio from './components/FooterPortfolio.vue'
+</script>
+
+<template>
+  <body>
+    <HeaderPortfolio />
+    <RouterView />
+    <FooterPortfolio />
+  </body>
+</template>
+
---
+++ src/components/HeaderPortfolio.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><RouterLink to="/">Blog</RouterLink></li>
+        <li><RouterLink to="/works">Works</RouterLink></li>
+        <li><RouterLink to="/contact">Contact</RouterLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+<style scoped>
+header ul li {
+  text-align: center;
+  padding: 10px 0 10px 0;
+  margin: 0px 20px 0px 20px;
+}
+@media (min-width: 900px) {
+  header ul {
+    display: flex;
+    justify-content: right;
+  }
+
+  header ul li {
+    padding: 20px 0 20px 0;
+  }
+}
+</style>
